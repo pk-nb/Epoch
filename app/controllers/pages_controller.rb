5a0df8659e7598b252bffc72007d7ef5117d7252
isaachermens <Isaac.Hermens@gmail.com>
2014-09-26 11:04:23 -0400

    Home page will no longer explode for guest users.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   def index
     @app_name = 'Epoch'
-    @events = current_user.events.all.sort_by { |el| el.start_date}
+    @events = if current_user then
+                current_user.events.all.sort_by { |el| el.start_date}
+                else Array.new
+              end
   end
 end
