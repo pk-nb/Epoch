c3065f47cd694ac58eb0f8d3e8a1084730439f1d
isaachermens <Isaac.Hermens@gmail.com>
2014-09-26 16:19:32 -0400

    Cleaned up if statement.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,10 @@
 class PagesController < ApplicationController
   def index
     @app_name = 'Epoch'
-    @events = if current_user then
+    @events = if current_user
                 current_user.events.all.sort_by { |el| el.start_date}
-                else Array.new
+              else
+                Array.new
               end
   end
 end
