c9318869d2ec28ed9e6451a586e267d73143d084
isaachermens <Isaac.Hermens@gmail.com>
2014-10-03 16:39:59 -0400

    Improved event validation.
    Cleaned up/improved event CRUD operations and functionality.
    
    Includes fixes for problems created by making events a nested resource.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,14 +4,21 @@ class Event < ActiveRecord::Base
   
   attr_accessor :start_date_pretty, :end_date_pretty
   # Chicken or Egg bauk bauk bauk
+  # For now, the rule that an event must belong to at least one timeline will be enforced
+  #  by the UI. Enforcing it at the model level is made difficult by the join table
   #validates :timelines, length: { minimum: 1 }
   validates_presence_of :title, :content, :start_date
+  validates_datetime :end_date, on_or_after: :start_date, allow_nil: true
 
   def start_date_pretty
     start_date.strftime('%B %d, %Y')
   end
 
   def end_date_pretty
-    end_date.strftime('%B %d, %Y')
+    unless end_date.nil?
+      end_date.strftime('%B %d, %Y')
+    else
+      'Ongoing'
+    end
   end
 end
