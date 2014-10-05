2c20f93ede81d66480cc8cb83966e47ab78e5805
isaachermens <Isaac.Hermens@gmail.com>
2014-10-05 17:14:22 -0400

    Configure events controller to respond to ajax. Will likely need to be modified when we know more about how we want to handle create/update forms.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,37 +1,63 @@
 class EventsController < ApplicationController
   def index
     @events = timeline.events
+
+    respond_to do |format|
+      format.html
+      format.json {render json: @events}
+    end
   end
   
   def show
     @event = timeline.events.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json {render json: @event}
+    end
   end
 
   def new
     @timeline = timeline
     @event = Event.new
+    respond_to do |format|
+      format.html
+      format.json {render json: @event}
+    end
   end
 
   def create
     timeline.events.create(event_params.merge(user_id: current_user.id))
-    redirect_to timeline_events_path
+    respond_to do |format|
+      format.html { redirect_to timeline_events_path }
+      format.json {render json: @event}
+    end
   end
 
   def edit
     @timeline = timeline
     @event = current_user.events.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @event }
+    end
   end
 
   def update
     current_user.events.find(params[:id]).tap do |event|
       event.update!(event_params)
     end
-    redirect_to timeline_event_path
+    respond_to do |format|
+      format.html { redirect_to timeline_event_path }
+      format.json { render json: @event }
+    end
   end
 
   def destroy
-    current_user.events.destroy(params[:id])
-    redirect_to root_path
+    event = current_user.events.destroy(params[:id])
+    respond_to do |format|
+      format.html {redirect_to timeline_events_path}
+      format.json
+    end
   end
 
   private
