080ffeb2bdda6bccdca3b43e77120d470a9df7a8
isaac.hermens@gmail.com <isaac.hermens@gmail.com>
2014-10-08 13:07:28 -0400

    Rebased on master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141005155417) do
+ActiveRecord::Schema.define(version: 20141005220541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 20141005155417) do
   create_table "events_timelines", id: false, force: true do |t|
     t.integer "event_id"
     t.integer "timeline_id"
+  end
+
+  create_table "profiles", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "favorite_cake"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "timelines", force: true do |t|
@@ -58,6 +68,8 @@ ActiveRecord::Schema.define(version: 20141005155417) do
   end
 
   add_foreign_key "events", "users", name: "event_user_foreign_key", dependent: :delete
+
+  add_foreign_key "profiles", "users", name: "profiles_user_id_fk", dependent: :delete
 
   add_foreign_key "timelines", "users", name: "timeline_user_foreign_key", dependent: :delete
 
