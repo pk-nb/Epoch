ebeb32596ee0ac2cf4da6740ec49dc905645acbf
isaac.hermens@gmail.com <isaac.hermens@gmail.com>
2014-10-12 12:56:12 -0400

    Users can now create Epoch accounts by providing an email address and password and login using those accounts.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,32 @@
 class SessionsController < ApplicationController
   def new
+    @session = Session.new
   end
 
   def create
-    user = User.from_omniauth(env['omniauth.auth'])
-    session[:user_id] = user.id
-    redirect_to root_path
+    unless params[:provider].nil?
+      user = User.from_omniauth(env['omniauth.auth'])
+      session[:user_id] = user.id
+    else
+      @session = Session.new(session_params)
+      if @session.valid?
+        self.current_user = @session.user
+        redirect_to root_path
+      else
+        # todo how do I make this error show up?
+        flash.now[:error] = 'The email or password is invalid'
+        render :new
+      end
+    end
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:login, :password)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find params[:id]
     redirect_to @user.profile
   end
 
@@ -24,18 +23,14 @@ class UsersController < ApplicationController
   end
 
   # POST /users
-  # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.new(user_params.merge(provider: 'Epoch'))
+    if @user.save
+      self.current_user = @user
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Unable to create account. Please correct the fields below'
+      render :new
     end
   end
 
@@ -71,6 +66,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params[:user]
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
