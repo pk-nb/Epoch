1bc27f67fe324e1cc07ecd985ef512a70fc50fd0
isaachermens <Isaac.Hermens@gmail.com>
2014-10-14 11:45:10 -0400

    Added user model tests for internal accounts.
    Commented out user request/routing tests for now.

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -3,9 +3,11 @@ require "rails_helper"
 RSpec.describe UsersController, :type => :routing do
   describe "routing" do
 
+=begin
     it "routes to #index" do
       expect(:get => "/users").to route_to("users#index")
     end
+=end
 
     it "routes to #new" do
       expect(:get => "/users/new").to route_to("users#new")
@@ -15,9 +17,11 @@ RSpec.describe UsersController, :type => :routing do
       expect(:get => "/users/1").to route_to("users#show", :id => "1")
     end
 
+=begin
     it "routes to #edit" do
       expect(:get => "/users/1/edit").to route_to("users#edit", :id => "1")
     end
+=end
 
     it "routes to #create" do
       expect(:post => "/users").to route_to("users#create")
@@ -27,9 +31,11 @@ RSpec.describe UsersController, :type => :routing do
       expect(:put => "/users/1").to route_to("users#update", :id => "1")
     end
 
+=begin
     it "routes to #destroy" do
       expect(:delete => "/users/1").to route_to("users#destroy", :id => "1")
     end
+=end
 
   end
 end
