c8877e4ace49a59cfc21d266b257cb99ea54e2c8
Nathanael Beisiegel <pknb.dev@gmail.com>
2014-10-20 22:21:27 -0400

    Precompile app.js too

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,9 @@ module Epoch
     # Enable gzip
     config.middleware.use Rack::Deflater
 
+    # Precompile more files
+    config.assets.precompile += %w( app.js )
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
