9e431957cfc921523c94a27b134bb12530c1918e
isaachermens <Isaac.Hermens@gmail.com>
2014-10-24 18:26:18 -0400

    Change to github dev app.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,10 +6,12 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :facebook, '322861781229311', '87c324c74416c2a57b9fa770e92a6e8f',
            {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
-  
+
   provider :twitter, 'Z6ZnqELqIAds2p9RM9tsu0ADf', 'Epq7T8mJvvUpAMrqpAvZkx4aFdLWRqZRPVAl4r7WACyLvjog1X',
            {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
 
-  provider :github, '4a1e7283b9233feae152', '85e795ea993cc64c2abea5f8ab77d1bce5cee5d3',
-           {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
+  #provider :github, '4a1e7283b9233feae152', '85e795ea993cc64c2abea5f8ab77d1bce5cee5d3',
+  #       {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
+  provider :github, '0277cf29d8fd96eadf2c', '65abf7f423dd201c935bc54b1e59b02d3ddbb177',
+         {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
 end
