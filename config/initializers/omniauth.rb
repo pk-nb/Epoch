df2048f5aba7160a1dd479aa1a295b93b3a7ae1b
isaachermens <Isaac.Hermens@gmail.com>
2014-10-24 18:26:18 -0400

    Change to github dev app.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -11,5 +11,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
 
   provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'],
+  #       {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
+  provider :github, '0277cf29d8fd96eadf2c', '65abf7f423dd201c935bc54b1e59b02d3ddbb177',
          {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
 end
