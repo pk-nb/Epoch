2209cf13de24e83e7b96dbbb34d158c7388026c7
isaachermens <Isaac.Hermens@gmail.com>
2014-10-24 21:15:11 -0400

    Moved API keys/ids into .env file (will use environment variables on server most likely)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,17 +1,15 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '600492169048-aik1d57ijkuhpk2grll471p4ejo1c1u2.apps.googleusercontent.com',
-           'PkWPes5xNEyR9QE69YvF16IL', {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
+  provider :google_oauth2, ENV['GOOGLE_ID'],
+           ENV['GOOGLE_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
 
-  provider :facebook, '322861781229311', '87c324c74416c2a57b9fa770e92a6e8f',
+  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'],
            {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
 
-  provider :twitter, 'Z6ZnqELqIAds2p9RM9tsu0ADf', 'Epq7T8mJvvUpAMrqpAvZkx4aFdLWRqZRPVAl4r7WACyLvjog1X',
+  provider :twitter, ENV['TWITTER_ID'], ENV['TWITTER_SECRET'],
            {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
 
-  #provider :github, '4a1e7283b9233feae152', '85e795ea993cc64c2abea5f8ab77d1bce5cee5d3',
-  #       {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
-  provider :github, '0277cf29d8fd96eadf2c', '65abf7f423dd201c935bc54b1e59b02d3ddbb177',
+  provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'],
          {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
 end
