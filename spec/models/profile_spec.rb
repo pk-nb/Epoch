08b4eb0a159de1c88a39699b262722432376d012
isaachermens <Isaac.Hermens@gmail.com>
2014-10-28 12:09:04 -0400

    Removed everything to do "profiles". This information will now be stored in the user model.

--- spec/models/profile_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe Profile do
-  it {should validate_presence_of(:user_id)}
-
-  it 'has a valid factory' do
-    p = Profile.new(FactoryGirl.attributes_for :profile)
-  end
-end
