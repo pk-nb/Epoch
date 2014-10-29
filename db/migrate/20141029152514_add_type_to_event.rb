1f24aea6bf1dfa87a32c8b31e0bf4de61b310711
isaac.hermens@gmail.com <isaac.hermens@gmail.com>
2014-10-29 12:47:44 -0400

    Fix migration.

--- db/migrate/20141029152514_add_type_to_event.rb
+++ db/migrate/20141029152514_add_type_to_event.rb
@@ -1,6 +1,6 @@
 class AddTypeToEvent < ActiveRecord::Migration
   def change
-    add_column :events, :event_type, :string
+    add_column :events, :type, :string
 
     add_column :repo_events, :event_id, :integer
     remove_column :repo_events, :date_created, :datetime
