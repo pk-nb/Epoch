73bdd88d8c48b381a53f7d7a123d0b79706ed013
isaachermens <Isaac.Hermens@gmail.com>
2014-12-04 09:51:30 -0500

    Added better error handling to repository and twitter forms.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,9 +8,31 @@ class TweetsController < ApplicationController
   end
 
   def create
+    success, result = try_import
+    respond_to do |format|
+      format.json do
+        if success
+          render json: result
+        else
+          render json: {errors: [result]}, status: 422
+        end
+      end
+    end
+  end
+
+  private
+  def try_import
+    # verify timeline name provided
+    if params[:timeline_name].nil? || params[:timeline_name].empty?
+      return false, 'Please enter a timeline name'
+    end
     twitter_int = TwitterIntegration.new current_user.twitter_account
-    user_id = params.require(:user_id)
-    if user_id == 0 then user_id = params.require(:alternate_user_id) end
+    user_id = params[:user_id]
+    if user_id == 0 then user_id = params[:alternate_user_id] end
+    # verify user provided
+    if user_id == 0 || user_id.nil? || user_id.empty?
+      return false, 'Please select or enter a Twitter user'
+    end
     tweets = twitter_int.get_all_tweets(user_id)
     # Filter by start date, if provided
     filters = params[:filters]
@@ -28,26 +50,19 @@ class TweetsController < ApplicationController
     timeline = nil
     unless tweets.count == 0
       extreme_dates = get_extreme_dates(tweets)
-      timeline = current_user.timelines.create(title: params.require(:timeline_name), content:
+      timeline = current_user.timelines.create(title: params[:timeline_name], content:
           "A Timeline of Tweets by #{user_id}", start_date: extreme_dates[:min], end_date: extreme_dates[:max])
       tweets.each do |tweet|
         # todo IJH: use <br> tags instead of newlines? Remove altogether? Create separate model?
         timeline.events.create(user_id: current_user.id, title: 'Tweet', content: "#{tweet.full_text}\nFavorited #{tweet.favorite_count} times\nRetweeted #{tweet.retweet_count} times",
-                                       start_date: tweet.created_at, end_date: tweet.created_at, event_type: 'Tweet')
+                               start_date: tweet.created_at, end_date: tweet.created_at, event_type: 'Tweet')
       end
+    else
+      return false, 'No tweets found for the specified user and date range'
     end
-    respond_to do |format|
-      format.json do
-        if tweets.count == 0
-          render json: {errors: ['No tweets found for the specified user and filter dates']}, status: 422
-        else
-          render json: timeline
-        end
-      end
-    end
+    return true, timeline
   end
 
-  private
   # Inspects an array of tweets
   # returns the earliest and latest dates found
   def get_extreme_dates(tweets)
